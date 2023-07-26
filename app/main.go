b581d9c92cd719b86349c696c6881ccd274522d7
Gherbust <gherbust@gmail.com>
2023-07-25 21:13:20 -0600

    contraseña

--- app/main.go
+++ app/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "github.com/gherbust-meli/lab/internal/shared"
-
 func main() {
 	// shared.Multiplicacion(889)
 
-	lista := []int{3, 4, 56, 45, 23, 89, 97, 567, 455, 334, 3345, 2, 4, 6}
-	shared.ObtenerLimites(lista)
+	// lista := []int{3, 4, 56, 45, 23, 89, 97, 567, 455, 334, 3345, 2, 4, 6}
+	// shared.ObtenerLimites(lista)
+	// contraseña := shared.CrearContraseña(8)
+	// fmt.Println(contraseña)
+
 }
---
+++ internal/shared/contraseña.go
@@ -0,0 +1,24 @@
+package shared
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func CrearContraseña(tamaño int) string {
+	letras := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/", "-", "+", "*", "#", "%", "$", "_", "@"}
+
+	var contraseña strings.Builder
+
+	rand.Seed(time.Now().Unix())
+
+	for i := 0; i < tamaño; i++ {
+		random := rand.Intn(len(letras))
+		letra := letras[random]
+		contraseña.WriteString(letra)
+	}
+	return contraseña.String()
+}
+
+//arreglo con todos lo caracteres
