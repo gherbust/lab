4a37775b6044902fe3bc398f9e2ebbec8dfd58f7
Dante Hazel Chavez Trinidad <dantetrinidad12@aragon.unam.mx>
2023-07-28 00:39:41 -0600

    SQL

---
+++ sql/queries.sql
@@ -0,0 +1,25 @@
+INSERT INTO `directorio`.`contacto`
+(
+`name`,
+`phone_number`,
+`email`,
+`last_update`,
+`enabled`)
+VALUES
+(
+"Dante",
+"1234567890",
+"dante@correo.com",
+now(),
+1);
+
+UPDATE `directorio`.`contacto`
+SET
+`enabled` = "0",
+`last_update` = now()
+WHERE `idcontacto` = 5;
+
+SELECT * FROM directorio.contacto where name = "Dante" and enabled = 1;
+SELECT name as nombre, phone_number as phone, email as correo FROM directorio.contacto order by name desc ;
+-- delete FROM directorio.contacto where name = "Dante" and enabled = 1;
+--SELECT name as nombre, phone_number as phone, email as correo FROM directorio.contacto where name = "Dante" and enabled = 1;  
