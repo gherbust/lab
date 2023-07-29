5a4bd17d661e7a044676753c3849d1dd06737c57
Gherbust <gherbust@gmail.com>
2023-07-28 22:03:49 -0600

    Update main.go

--- app/main.go
+++ app/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"github.com/gherbust/lab/internal/directory/applications"
+	"github.com/gherbust/lab/internal/directory/infrastructure"
 	"github.com/gherbust/lab/internal/shared"
+	stringsfunctionsinfrastructure "github.com/gherbust/lab/internal/stringsfunctions/infrastructure"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	directory := applications.NewDirectory()
+	handler := infrastructure.NewDirectoryHandler(*directory)
+	r := gin.Default()
+	r.POST("/directory", handler.Create)
+	r.GET("/directory/:name", handler.GetByName)
+	r.GET("/directory", handler.GetAll)
+
+	r.POST("/stringConverter", stringsfunctionsinfrastructure.StringConverter)
+
+	r.Run()
+
 	// shared.Multiplicacion(889)
 
 	// lista := []int{3, 4, 56, 45, 23, 89, 97, 567, 455, 334, 3345, 2, 4, 6}
